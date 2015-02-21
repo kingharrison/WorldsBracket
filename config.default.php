28b231643ca2620b447fcb744fd374f0c3413f4c
kingharrison <king4@k3s.com>
2015-02-21 11:33:16 -0500

    V1

---
+++ config.default.php
@@ -0,0 +1,10 @@
+<?php
+
+$config['adminUserIds'] = '';
+$config['submissionEndDate'] = '';
+$config['dbUserName'] = 'user';
+$config['dbPassword'] = 'pass';
+$config['dbHostName'] = 'ip';
+$config['compYear'] = ;
+
+?>
